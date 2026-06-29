706cca678b13cb393ebdb99d9ff12fad5e33ab08
alim <itbze.company@gmail.com>
2026-06-29 19:16:18 +0000

    feat(motion): hero entrance cascade, scroll-reveal sections; add README

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,21 @@ import { Hero } from '@/components/home/Hero'
 import { BenefitsBar } from '@/components/home/BenefitsBar'
 import { PopularSection } from '@/components/home/PopularSection'
 import { NewsletterSection } from '@/components/home/NewsletterSection'
+import { Reveal } from '@/components/ui/Reveal'
 
 export function HomePage() {
   return (
     <>
       <Hero />
-      <BenefitsBar />
-      <PopularSection />
-      <NewsletterSection />
+      <Reveal>
+        <BenefitsBar />
+      </Reveal>
+      <Reveal>
+        <PopularSection />
+      </Reveal>
+      <Reveal>
+        <NewsletterSection />
+      </Reveal>
     </>
   )
 }
